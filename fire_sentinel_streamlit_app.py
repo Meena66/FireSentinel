34f3aa46c6a7d8a5fa26f57394952c21a268524b
Meena66 <meena80183@gmail.com>
2025-07-09 20:19:17 +0530

    Update fire_sentinel_streamlit_app.py

--- fire_sentinel_streamlit_app.py
+++ fire_sentinel_streamlit_app.py
@@ -9,32 +9,22 @@ st.title("🔥 FireSentinel - Wildfire Prediction App")
 st.write("Predict wildfire risk using the trained Random Forest model.")
 
 ndvi = st.slider("NDVI", 0.0, 1.0, 0.5)
+prev_fire = st.selectbox("Previous Fire (Yes=1, No=0)", options=[0.0, 1.0])
+elevation = st.slider("Elevation", 0.0, 3000.0, 500.0)
+erc = st.slider("ERC", 0.0, 100.0, 20.0)
+pdsi = st.slider("PDSI", -5.0, 5.0, 0.0)
+population = st.slider("Population Density", 0.0, 10000.0, 100.0)
+pr = st.slider("Precipitation", 0.0, 50.0, 0.0)
+sph = st.slider("Specific Humidity", 0.0, 1.0, 0.5)
+th = st.slider("TH Index", 250.0, 350.0, 300.0)
 tmmn = st.slider("Minimum Temperature (°C)", -20.0, 50.0, 15.0)
 tmmx = st.slider("Maximum Temperature (°C)", -10.0, 60.0, 35.0)
-sph = st.slider("Specific Humidity", 0.0, 1.0, 0.5)
 vs = st.slider("Wind Speed", 0.0, 20.0, 3.0)
-pdsi = st.slider("PDSI", -5.0, 5.0, 0.0)
-pr = st.slider("Precipitation", 0.0, 50.0, 0.0)
-th = st.slider("TH Index", 250.0, 350.0, 300.0)
-elevation = st.slider("Elevation", 0.0, 3000.0, 500.0)
-population = st.slider("Population Density", 0.0, 10000.0, 100.0)
-erc = st.slider("ERC", 0.0, 100.0, 20.0)
-prev_fire = st.selectbox("Previous Fire (Yes=1, No=0)", options=[0.0, 1.0])
 
-input_data = pd.DataFrame([{
-    'NDVI': ndvi,
-    'tmmn': tmmn,
-    'tmmx': tmmx,
-    'sph': sph,
-    'vs': vs,
-    'pdsi': pdsi,
-    'pr': pr,
-    'th': th,
-    'elevation': elevation,
-    'population': population,
-    'erc': erc,
-    'PrevFireMask': prev_fire
-}])
+input_data = pd.DataFrame([[
+    ndvi, prev_fire, elevation, erc, pdsi, population,
+    pr, sph, th, tmmn, tmmx, vs
+]], columns=['NDVI', 'PrevFireMask', 'elevation', 'erc', 'pdsi', 'population', 'pr', 'sph', 'th', 'tmmn', 'tmmx', 'vs'])
 
 if st.button("Predict Fire Risk"):
     try:
